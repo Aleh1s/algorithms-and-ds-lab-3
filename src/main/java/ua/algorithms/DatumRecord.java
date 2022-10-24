860c2631e3f5593a60fe4e9f7266a1c6f71de047
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-10-24 16:41:00 +0300

    add datum record size constant

--- src/main/java/ua/algorithms/DatumRecord.java
+++ src/main/java/ua/algorithms/DatumRecord.java
@@ -9,6 +9,8 @@ public class DatumRecord {
 
     private long id;
     private String value;
+    public static final int VALUE_BYTES = 255;
+    public static final int DATUM_RECORD_BYTES = Long.BYTES + VALUE_BYTES;
 
     public void setValue(String value) {
         if (value.length() > 255)
