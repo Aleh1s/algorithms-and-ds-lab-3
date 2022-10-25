269bf7fa6536c93e3223a1aa9c983871b1cbbd70
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-10-25 18:50:00 +0300

    refactoring

--- src/main/java/ua/algorithms/Main.java
+++ src/main/java/ua/algorithms/Main.java
@@ -1,7 +1,26 @@
 package ua.algorithms;
 
+import ua.algorithms.accessor.FileAccessor;
+import ua.algorithms.accessor.GlobalFileAccessor;
+import ua.algorithms.accessor.IndexFileAccessor;
+import ua.algorithms.repository.SimpleRepository;
+import ua.algorithms.structure.DatumRecord;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        IndexFileAccessor indexFileAccessor =
+                (IndexFileAccessor) FileAccessor.of("src/main/resources/index.bin", "INDEX");
+        GlobalFileAccessor globalFileAccessor =
+                (GlobalFileAccessor) FileAccessor.of("src/main/resources/global.bin", "GLOBAL");
 
+        SimpleRepository simpleRepository = new SimpleRepository(
+                indexFileAccessor,
+                globalFileAccessor
+        );
+
+        DatumRecord datumRecord = simpleRepository.find(100);
+        System.out.println(datumRecord);
     }
 }
--- src/main/java/ua/algorithms/structure/IndexBlock.java
+++ src/main/java/ua/algorithms/structure/IndexBlock.java
@@ -13,8 +13,8 @@ public class IndexBlock {
     private List<IndexRecord> records;
     public static final int SIZE_OFFSET = 0;
     public static final int SIZE_BYTES = Integer.BYTES;
-    public static final int RECORDS_BYTES = 1020;
     public static final int RECORDS_OFFSET = SIZE_OFFSET + SIZE_BYTES;
+    public static final int RECORDS_BYTES = 1024;
     public static final int BYTES = SIZE_BYTES + RECORDS_BYTES; // size of block in bytes
 
     public boolean addRecord(IndexRecord indexRecord) {
